4baca2ca4e297d0f9fa1878e253382f59d601f6a
Akhilesh <95014375+AkhileshGurmit@users.noreply.github.com>
2025-03-13 10:12:22 +0530

    Update output.tf

--- cloudfront-cdn/output.tf
+++ cloudfront-cdn/output.tf
@@ -2,3 +2,8 @@ output "cloudfront_domain_name" {
   description = "CloudFront Distribution Domain Name"
   value       = aws_cloudfront_distribution.cdn.domain_name
 }
+
+output "oai_arn" {
+  description = "The ARN of the CloudFront Origin Access Identity"
+  value       = aws_cloudfront_origin_access_identity.oai.iam_arn
+}
